d8530885034bb207bf22fa98f735ba5b99cfd90b
arturchichorro <arturchichorro@gmail.com>
2024-07-08 17:27:19 +0100

    Fixed footer bug

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -21,9 +21,7 @@ export function SiteFooter() {
                     </a>
                 </div>
                 <div className="mb-2 flex space-x-2 text-sm text-muted-foreground">
-                    <a href={siteConfig.links.personalSite} target="_blank">
-                        {siteConfig.author}
-                    </a>
+                    {siteConfig.author}
                 </div>
             </div>
         </footer>
