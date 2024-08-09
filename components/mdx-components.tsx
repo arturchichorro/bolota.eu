97c21d0f2d298da91462c56813b106830ab28ff8
arturchichorro <arturchichorro@gmail.com>
2024-08-09 13:13:20 +0100

    Added possibility to add captions to pictures in three-medias

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import * as runtime from "react/jsx-runtime";
 import { Callout } from "./callout";
-import { ThreeMedias } from "./three-pics";
+import { ThreeMedias } from "./three-medias";
 import { cn } from "@/lib/utils";
 
 const useMDXComponent = (code: string) => {
