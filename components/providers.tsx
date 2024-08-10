597e3a69955f064308ae317fb1a750829d4d1824
arturchichorro <arturchichorro@gmail.com>
2024-08-10 11:44:57 +0100

    Fixed client not match server classNames error with theme provider

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,9 +1,21 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
-import { ReactNode } from "react";
+import { ReactNode, useState, useEffect } from "react";
 
 export function Providers({ children }: { children: ReactNode }) {
+
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true)
+    }, []);
+
+    if (!mounted) {
+        return <>{children}</>
+    }
+
+
     return (
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
             {children}
