c0bf9dec71f51d8eb4fdaf5218369fa7d89bdc40
arturchichorro <arturchichorro@gmail.com>
2024-09-26 23:05:37 +0100

    Final touches to mobile post item styling

--- components/post-item.tsx
+++ components/post-item.tsx
@@ -9,18 +9,20 @@ interface PostItemProps {
 }
 
 export function PostItem({ slug, title, description, date }: PostItemProps) {
+
+
     return (
-        <article className="flex flex-row justify-between border-border border-b py-2">
-            <div>
+        <article className="grid sm:grid-cols-3 grid-cols-4 border-border border-b py-2">
+            <div className="sm:col-span-2 col-span-3">
                 <h2 className="link tracking-widest">
                     <Link href={slug}># {title}</Link>
                 </h2>
             </div>
-            <div className="flex justify-between items-center">
-                <dl className="flex flex-row">
+            <div className="col-span-1 text-right">
+                <dl>
                     <dt className="sr-only">Published On</dt>
-                    <dd className="flex text-sm sm:text-xs">
-                        <time className=""dateTime={date}>{formatDate(date)}</time>
+                    <dd className="text-sm">
+                        <time dateTime={date}>{formatDate(date)}</time>
                     </dd>
                 </dl>
             </div>
--- lib/utils.ts
+++ lib/utils.ts
@@ -8,8 +8,9 @@ export function cn(...inputs: ClassValue[]) {
 
 export function formatDate(input: string | number): string {
   const date = new Date(input);
+
   return date.toLocaleDateString("en-US", {
-    month: "long",
+    month: "short",
     day: "numeric",
     year: "numeric",
   });
