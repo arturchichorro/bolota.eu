e9ac74bf4521651e754dace333f9a6904b784860
arturchichorro <arturchichorro@gmail.com>
2024-11-02 12:26:42 +0000

    Added projects to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,13 @@ import { cn, sortPosts } from "@/lib/utils";
 import { posts } from "#site/content";
 import Link from "next/link";
 import { PostItem } from "@/components/post-item";
+import { ProjectItem } from "@/components/project-item";
+import { projects } from "@/lib/projects";
 
 export default function Home() {
 
-  const latestPosts = sortPosts(posts).filter(post => post.published).slice(0, 10);
+  const latestPosts = sortPosts(posts).filter(post => post.published).slice(0, 5);
+  const topProjects = projects.slice(0,6);
 
   return (
     <>
@@ -21,22 +24,42 @@ export default function Home() {
           </p>
         </div>
       </section>
-      <section className="container max-w-4xl flex flex-col pt-6">
-        <h2 className="text-2xl font-black text-accent py-2">
-          ** Latest Posts.
-        </h2>
-        <ul className="flex flex-col gap-2">
-          {latestPosts.map((post) => (
-            <li key={post.slug}>
-              <PostItem
-                slug={post.slug}
-                title={post.title}
-                description={post.description}
-                date={post.date}
-              />
-            </li>
-          ))}
-        </ul>
+      <section className="container max-w-4xl flex flex-col py-6 gap-10">
+        <div>
+          <h2 className="text-2xl font-black text-accent py-2">
+            ** Latest Posts.
+          </h2>
+          <ul className="flex flex-col gap-2">
+            {latestPosts.map((post) => (
+              <li key={post.slug}>
+                <PostItem
+                  slug={post.slug}
+                  title={post.title}
+                  description={post.description}
+                  date={post.date}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <h2 className="text-2xl font-black text-accent py-2">
+            ** Projects.
+          </h2>
+          <ul className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+            {topProjects.map((project, index) => (
+              <li key={index}>
+                <ProjectItem
+                  title={project.title} 
+                  description={project.description} 
+                  years={project.years} 
+                  gh={project.gh} 
+                  posts={project.posts}  
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
       </section>
     </>
   );
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,100 +1,11 @@
 import { Metadata } from "next";
 import { ProjectItem } from "@/components/project-item";
+import { projects } from "@/lib/projects";
 
 export const metadata: Metadata = {
     title: "bolotaeu Projects",
     description: "List of all relevant projects built by arturchichorro.",
 }
-
-const projects = [
-    {
-        title: "Bolota.eu",
-        description: "This website's source code.",
-        years: "2023-2024",
-        gh: "https://github.com/arturchichorro/bolota.eu",
-        posts: [
-            {post_title: "Version 2", external: false, url: "posts/6_bolotaeuv2"},
-            {post_title: "Version 1", external: false, url: "posts/1_bolotaeuv1"},
-        ]
-    },
-    {
-        title: "Basketball Vision",
-        description: "Counting attempted/made Basketball shots w/ Python.",
-        years: "2024",
-        gh: "https://github.com/arturchichorro/bballvision",
-        posts: [
-            {post_title: "Implementation", external: false, url: "posts/10_bballvision"},
-            {post_title: "Idea", external: false, url: "posts/7_sharing_and_buildspace"},
-        ]
-    },
-    {
-        title: "Walkie Talkie",
-        description: "Simple mobile app to send voice messages.",
-        years: "2024",
-        gh: "https://github.com/arturchichorro/WalkieTalkie",
-        posts: [
-            {post_title: "Article", external: false, url: "posts/11_walkietalkie"},
-        ]
-    },
-    {
-        title: "Spotify Controller",
-        description: "Device to pause/play/skip/like songs on spotify.",
-        years: "2024",
-        gh: "https://github.com/arturchichorro/Spotify_Controller",
-        posts: [
-            {post_title: "Article", external: false, url: "posts/8_embedded_programming"},
-        ]
-    },
-    {
-        title: "Learn Anything (Lanny)",
-        description: "Turn any text document into a Quizz using AI.",
-        years: "2024",
-        gh: undefined,
-        posts: [
-            {post_title: "Article", external: false, url:"posts/5_shift_appens"},
-            {post_title: "Behance", external: true, url:"https://www.behance.net/gallery/198170575/Lanny"}
-        ]
-    },
-    {
-        title: "Rust Chess Engine",
-        description: "Work in Progress: Building a Chess Engine with Rust.",
-        years: "2024",
-        gh: "https://github.com/arturchichorro/chess-engine",
-        posts: [
-            {post_title: "Version 1", external: false, url:"posts/4_chess_enginev1"},
-        ]
-    },
-    {
-        title: "Connect Four Bot + UI",
-        description: "Play Connect Four against the computer.",
-        years: "2024",
-        gh: undefined,
-        posts: [
-            {post_title: "Play here", external: false, url:"posts/9_connect4"},
-        ]
-    },
-    {
-        title: "React Chess Interface",
-        description: "Simple local chess implementation using React.",
-        years: "2024",
-        gh: "https://github.com/arturchichorro/chess-react",
-        posts: [
-            {post_title: "Play here", external: true, url:"https://chess-react-bolota.netlify.app/"},
-            {post_title: "Article", external: false, url:"posts/2_reactchessv1"},
-        ]
-    },
-    {
-        title: "Mathematical Animations",
-        description: "Source code for the videos I made with Manim.",
-        years: "2024",
-        gh: "https://github.com/arturchichorro/manim_videos",
-        posts: [
-            {post_title: "Learning Manim", external: false, url:"posts/3_manim_first_attempt"},
-            {post_title: "Instagram", external: true, url:"https://www.instagram.com/p/C3F2XjuN8qU/"},
-        ]
-    },
-]
-
 
 export default async function AboutPage() {
     return (
