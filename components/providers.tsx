ecdc9722835cc69eb3a180a90fc7e9fda0439db3
arturchichorro <arturchichorro@gmail.com>
2025-02-07 20:39:53 +0000

    posthog pageview

--- components/providers.tsx
+++ components/providers.tsx
@@ -3,29 +3,25 @@
 import posthog from "posthog-js";
 import { PostHogProvider } from 'posthog-js/react';
 import { ThemeProvider } from "next-themes";
-import { ReactNode } from "react";
-
-if (typeof window !== 'undefined') {
-  const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
-  if (posthogKey) {
-      posthog.init(posthogKey, {
-          api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://eu.i.posthog.com",
-          person_profiles: "identified_only",
-          loaded: (posthog) => {
-              if (process.env.NODE_ENV === "development") posthog.debug();
-          },
-      });
-  } else {
-      if (!posthogKey) {
-          console.error("PostHog key is not defined");
-      }
-  }
-}
+import { ReactNode, useEffect } from "react";
+import PostHogPageView from "./posthog/PostHogPageView"
 
 export function Providers({ children }: { children: ReactNode }) {
+  useEffect(() => {
+    const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
+    if (posthogKey) {
+      posthog.init(posthogKey, {
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+        capture_pageview: false,
+        capture_pageleave: true,
+      });
+    }
+  }, []);
+  
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
       <PostHogProvider client={posthog}>
+        <PostHogPageView />
         {children}
       </PostHogProvider>
     </ThemeProvider>
