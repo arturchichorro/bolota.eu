cb21bb3f31c51ef4ed86d1856601034b9857fa7a
arturchichorro <arturchichorro@gmail.com>
2025-02-07 20:17:50 +0000

    changes to providers

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import posthog from "posthog-js"
-import { PostHogProvider } from 'posthog-js/react'
+import posthog from "posthog-js";
+import { PostHogProvider } from 'posthog-js/react';
 import { ThemeProvider } from "next-themes";
 import { ReactNode } from "react";
 
@@ -9,23 +9,25 @@ if (typeof window !== 'undefined') {
     const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
     if (posthogKey) {
         posthog.init(posthogKey, {
-          api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://eu.i.posthog.com",
-          person_profiles: "identified_only",
-          loaded: (posthog) => {
-            if (process.env.NODE_ENV === "development") posthog.debug();
-          },
+            api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://eu.i.posthog.com",
+            person_profiles: "identified_only",
+            loaded: (posthog) => {
+                if (process.env.NODE_ENV === "development") posthog.debug();
+            },
         });
-      } else {
-        console.error("PostHog key is not defined");
-      }
-  }
+    } else {
+        if (!posthogKey) {
+            console.error("PostHog key is not defined");
+        }
+    }
+}
 
 export function Providers({ children }: { children: ReactNode }) {
     return (
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
         <PostHogProvider client={posthog}>
-            <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-                {children}
-            </ThemeProvider>
+          {children}
         </PostHogProvider>
-    )
+      </ThemeProvider>
+    );
 }
