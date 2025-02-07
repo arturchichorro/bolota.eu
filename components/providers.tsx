efb1ebb4f0ef52ba55f012d7974b7b778657d610
arturchichorro <arturchichorro@gmail.com>
2025-02-07 19:59:10 +0000

    setup posthog

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,12 +1,31 @@
 "use client";
 
+import posthog from "posthog-js"
+import { PostHogProvider } from 'posthog-js/react'
 import { ThemeProvider } from "next-themes";
 import { ReactNode } from "react";
 
+if (typeof window !== 'undefined') {
+    const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
+    if (posthogKey) {
+        posthog.init(posthogKey, {
+          api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://eu.i.posthog.com",
+          person_profiles: "identified_only",
+          loaded: (posthog) => {
+            if (process.env.NODE_ENV === "development") posthog.debug();
+          },
+        });
+      } else {
+        console.error("PostHog key is not defined");
+      }
+  }
+
 export function Providers({ children }: { children: ReactNode }) {
     return (
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-            {children}
-        </ThemeProvider>
+        <PostHogProvider client={posthog}>
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+                {children}
+            </ThemeProvider>
+        </PostHogProvider>
     )
 }
