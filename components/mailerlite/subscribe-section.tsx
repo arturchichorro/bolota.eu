8e87009154463892f64a2ac650a0cb94d0cebb7a
arturchichorro <arturchichorro@gmail.com>
2025-04-06 20:18:33 +0200

    changed font color of subscribe section

--- components/mailerlite/subscribe-section.tsx
+++ components/mailerlite/subscribe-section.tsx
@@ -4,7 +4,7 @@ const SubscribeSection = () => {
   return (
     <div className="bg-popover py-4 border-2 border-border rounded-lg max-w-md flex flex-col justify-center gap-3">
         <div className="text-balance text-center">
-            <span className="text-sm text-secondary">If you&apos;d like to get updates when I write something new, enter your email below!</span>
+            <span className="text-sm text-accent">If you&apos;d like to get updates when I write something new, enter your email below!</span>
         </div>
         <SubscriberForm />
     </div>
