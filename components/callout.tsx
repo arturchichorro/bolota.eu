4e155f9ee21f765b49cdafe2f16ed7076b9ff88e
arturchichorro <arturchichorro@gmail.com>
2025-11-13 19:02:32 +0100

    backup draft

--- components/callout.tsx
+++ components/callout.tsx
@@ -14,10 +14,10 @@ export function Callout({
 }: CalloutProps) {
     return (
         <div className={
-            cn("my-6 items-start rounded-md border border-l-4 p-4 w-full dark:max-w-none",
+            cn("items-start rounded-md border border-l-4 px-4 w-full",
                 {
-                    "border-red-900 bg-red-50 dark:prose": type === "danger",
-                    "border-yellow-900 bg-yellow-50 dark:prose": type === "warning",
+                    "border-red-900 bg-red-50": type === "danger",
+                    "border-yellow-900 bg-yellow-50": type === "warning",
                 }
             )}
             {...props}
