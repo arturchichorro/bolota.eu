e9a96dbb6efef96694a90f4f27f9e25d1b2352e5
arturchichorro <105810205+arturchichorro@users.noreply.github.com>
2026-09-12 13:48:26 +0200

    fix: synchronize initial Velite build

--- next.config.mjs
+++ next.config.mjs
@@ -10,15 +10,18 @@ export default {
 }
 
 class VeliteWebpackPlugin {
-    static started = false
+    static buildPromise
+
     apply(/** @type {import('webpack').Compiler} */ compiler) {
         // executed three times in nextjs
         // twice for the server (nodejs / edge runtime) and once for the client
         compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
-            if (VeliteWebpackPlugin.started) return
-            VeliteWebpackPlugin.started = true
-            const dev = compiler.options.mode === 'development'
-            await build({ watch: dev, clean: !dev })
+            if (!VeliteWebpackPlugin.buildPromise) {
+                const dev = compiler.options.mode === 'development'
+                VeliteWebpackPlugin.buildPromise = build({ watch: dev, clean: !dev })
+            }
+
+            await VeliteWebpackPlugin.buildPromise
         })
     }
 }
