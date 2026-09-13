ded8b4e3873f0c800c8d9c98274b502289453bd5
arturchichorro <105810205+arturchichorro@users.noreply.github.com>
2026-09-13 21:31:03 +0200

    chore: update article icon

--- components/svg/homeHubSvg.tsx
+++ components/svg/homeHubSvg.tsx
@@ -11,10 +11,11 @@ function HomeHubSVG({ size = 24 }: HomeHubIconProps) {
             viewBox="0 0 512 512"
             aria-hidden="true"
         >
-            <rect width="512" height="512" rx="112" fill="#201d1b" />
+            {/* <rect width="512" height="512" rx="112" fill="#201d1b" /> */}
             <path
                 d="M126 234 242 138Q256 126 270 138L386 234Q400 246 400 264v96q0 18-18 18H130q-18 0-18-18v-96q0-18 14-30Z"
                 fill="#e8753d"
+                transform="translate(256 256) scale(1.12) translate(-256 -256)"
             />
         </svg>
     );
